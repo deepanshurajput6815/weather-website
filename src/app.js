2fee518afe88e6d4220c481228ef859a0f4089d0
unknown <deepanshurajput3838@gmail.com>
2021-06-08 13:55:48 +0530

    all setup is done

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Deepanshu'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Deepanshu'
+        name: 'Andrew Mead'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Deepanshu'
+        name: 'Andrew Mead'
     })
 })
 
